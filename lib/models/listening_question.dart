a8a970a0bab72bc532ceb2fba414ae87a76f7c5b
DESKTOP-E2HFGD2\LBUP3 <k.ygster8264@gmail.com>
2025-02-02 18:43:21 +0900

    durationプロパティを削除

--- lib/models/listening_question.dart
+++ lib/models/listening_question.dart
@@ -4,12 +4,10 @@ class ListeningQuestion {
   final String audioPath;
   final String imagePath;
   final String englishText;
-  final int duration; // 音声の長さ（秒）
 
   const ListeningQuestion({
     required this.audioPath,
     required this.imagePath,
     required this.englishText,
-    required this.duration,
   });
 }
